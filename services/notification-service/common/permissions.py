287ade238fe920d8f668de01e6936efefc64a8b4
Henna Parveen <hennaparveen77@gmail.com>
2026-06-22 09:57:54 +0400

    feat(notification): add Notification model and initial migration

--- services/notification-service/common/permissions.py
+++ services/notification-service/common/permissions.py
@@ -1,7 +1,6 @@
+from django.conf import settings
 from rest_framework.permissions import BasePermission
-
 from .actors import KidActor, ParentActor
-
 
 class IsKid(BasePermission):
     def has_permission(self, request, view):
@@ -11,3 +10,8 @@ class IsKid(BasePermission):
 class IsParent(BasePermission):
     def has_permission(self, request, view):
         return isinstance(request.user, ParentActor)
+
+class IsInternalService(BasePermission):
+    def has_permission(self, request, view):
+        token = request.headers.get('X-Internal-Token')
+        return bool(token) and token == settings.INTERNAL_SERVICE_TOKEN
